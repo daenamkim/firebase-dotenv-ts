90b043a2dcbf197f5164bbebf208b240ea95d5e0
Daenam Kim <daenam.kim@gmail.com>
2022-05-11 14:59:31 +0900

    Add todo comments

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -7,15 +7,19 @@ chai.use(chaiHttp);
 
 describe("Dotenv", () => {
   it("should get a variable from process.env in test", () => {
+    // TODO: why does this fail?
     expect(process.env.WORK).to.equal("work-local");
   });
   it("should get a variable from import in test", () => {
+    // TODO: why does this fail?
     expect(config.WORK).to.equal("work-local");
   });
   it("should get a secret from process.env in test", () => {
+    // TODO: why does this fail?
     expect(process.env.SECRET).to.equal("secret-local");
   });
   it("should get a secret from import in test", () => {
+    // TODO: why does this fail?
     expect(config.WORK).to.equal("work-local");
   });
   it("should return with secret", async () => {
@@ -28,6 +32,7 @@ describe("Dotenv", () => {
     });
   });
   it("should return with no secret", async () => {
+    // TODO: why does this fail?
     const res = await chai.request("http://localhost:5001").get("/dotenv/us-central1/endpointWithNoSecret");
     expect(res.status).to.equal(200);
     expect(res.body).to.deep.equal({
